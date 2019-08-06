3c687eddc098bb65a20d4b26a2cbd3445d48bded
zhangxishuo <zhangxishuo1998@gmail.com>
2019-08-06 11:41:57 +0800

    init-eureka

---
+++ eureka/src/main/java/club/yunzhi/cloud/EurekaApplication.java
@@ -0,0 +1,18 @@
+package club.yunzhi.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 1. 只需要使用 @EnableEurekaServer 注解就可以让应用变为 Eureka Server
+ * 2. pom 文件中对应到 spring-cloud-starter-netflix-eureka-server 依赖
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
